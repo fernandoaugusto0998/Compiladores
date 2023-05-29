e474b8d24b87ac4d75ab09a43723b2829ebf5d89
Fernando Augusto <91295365+fernandoaugusto0998@users.noreply.github.com>
2023-05-29 14:21:24 -0400

    Add files via upload

---
+++ Linguagem CBR/CBR/src/Compilador.java
@@ -0,0 +1,61 @@
+import java.io.IOException;
+import java.util.List;
+
+public class Compilador {
+
+	public List<String> alfabeto;
+	public List<String> exemplo;
+	public List<String> expressoes;
+	public List<String> dicionario;
+	
+	public Compilador() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public void Iniciar() throws IOException {
+		String linha;
+		System.out.println("Iniciando o compilador...");
+		
+		LerArquivo leitor = new LerArquivo();
+		alfabeto = leitor.ler("C:\\Users\\Informatica\\eclipse-workspace\\CBR\\alfabeto.txt");
+		exemplo = leitor.ler("C:\\Users\\Informatica\\eclipse-workspace\\CBR\\exemplo.txt");
+		expressoes = leitor.ler("C:\\Users\\Informatica\\eclipse-workspace\\CBR\\expressoes.txt");
+		dicionario = leitor.ler("C:\\Users\\Informatica\\eclipse-workspace\\CBR\\dicionario.txt");
+		for( int i =0; i < alfabeto.size();i++) {
+			System.out.println(alfabeto.get(i));
+		}
+		System.out.println("--------------");
+		AnalisadorLexico lexico = new AnalisadorLexico();
+		for( int i =0; i < exemplo.size();i++) {
+			linha = exemplo.get(i);
+			lexico.analisar(linha, alfabeto);
+		}
+		AnalisadorSintatico sintatico = new AnalisadorSintatico();
+		for( int i =0; i < exemplo.size();i++) 
+		{
+			linha = exemplo.get(i);
+			sintatico.analisar(linha, expressoes);
+		}
+		
+		System.out.println("Analisando semanticamente");
+		AnalisadorSemantico semantico = new AnalisadorSemantico();
+		for( int i =0; i < exemplo.size();i++) 
+		{
+			linha = exemplo.get(i);
+			semantico.Analisar(linha, dicionario);
+		}
+		System.out.println("Fim da analise semantica");
+
+		GeradorByteCode gerador = new GeradorByteCode();
+        System.out.println("Gerando bytecode das linhas do arquivo de exemplo:");
+        String exemploJava = "public class Main {\n  public static void main(String[] args) {\n";
+        for (int i = 0; i < exemplo.size(); i++) {
+            linha = exemplo.get(i);
+            String bytecode = gerador.gerar(linha);
+            System.out.println("Bytecode gerado: " + bytecode);
+            exemploJava = exemploJava + "    " + bytecode + "\n";
+        }
+	}
+}
+
